cd77e5f3cd622912b1b83dd5f58e5e83aa80c3e1
zyH <hzy18023@gmail.com>
2024-06-12 11:10:04 +0800

    chore: init repo

---
+++ app.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+  import '@unocss/reset/tailwind.css'
+  
+</script>
+<template>
+  <div>
+    <nuxt-layout>
+      <nuxt-page />
+    </nuxt-layout>
+  </div>
+</template>
+
+<style>
+  body {
+    background-color: #FFFFFF;
+    color: rgba(0, 0, 0, 0.8);
+  }
+  
+  .dark body {
+    background-color: #091A28;
+    color: #EBF4F1;
+  }
+  
+  .sepia body {
+    background-color: #F1E7D0;
+    color: #433422;
+  }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const {x,y} = useMouse()
+const colorMode  = useColorMode()
+</script>
+
+<template>
+  <div text-red>
+    hello world {{ x }} - {{ y }}
+  </div>
+  <select v-model="colorMode.preference">
+     <option value="system">System</option>
+    <option value="light">Light</option>
+    <option value="dark">Dark</option>
+    <option value="sepia">sepia</option>
+  </select>
+</template>
+
+<style scoped>
+
+</style>
